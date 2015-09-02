c8d1bc5d6171a81e1b01e37a603f08e22aa02529
daroldg <dbgreenapp@gmail.com>
2015-09-02 11:16:26 -0600

    Added console message

--- TestProject01/TestSolution01/Program.cs
+++ TestProject01/TestSolution01/Program.cs
@@ -10,6 +10,8 @@ namespace TestSolution01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("blah");
+            Console.Read();
         }
 
         static void g() { }
